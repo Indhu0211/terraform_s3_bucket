6e580e35940e965b2a9a46b685f97bc39fb9628e
INDHUMATHI <138423507+Indhu0211@users.noreply.github.com>
2024-01-31 14:35:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,19 @@
 variable "region" {
-  description = "The AWS region where the S3 bucket will be created."
+  description = "The AWS region."
+  type        = string
+  default     = "us-east-2"
 }
 
 variable "bucket_name" {
   description = "The name of the S3 bucket."
+  type        = string
 }
-
 variable "bucket_acl" {
-  description = "The access control list (ACL) for the S3 bucket."
+  description = "The access control list (ACL) for the bucket."
   default     = "private"
 }
 
-variable "versioning" {
-  description = "Whether versioning should be enabled for the S3 bucket."
-  default     = false
+variable "versioning_enabled" {
+  description = "Whether versioning should be enabled for the bucket."
+  default     = "Enabled"
 }
